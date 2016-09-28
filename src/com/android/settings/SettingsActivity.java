f7b051e6ffebcdf5bb6c8e396d9052e71f8947e3
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-09-28 04:48:34 +0200

    Revert "Settings: Move KernelAdiutor to dashboard"
    This reverts commit 589bbcce2a6e830efec7c97c32eaf59135c1ef0d.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -222,8 +222,6 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private static final String SUPERSU_FRAGMENT = "com.android.settings.SuperSU";
 
-    private static final String KA_FRAGMENT = "com.android.settings.KernelAdiutor";
-
     private String mFragmentClass;
 
     private CharSequence mInitialTitle;
@@ -1017,13 +1015,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             finish();
             return null;
         }
-        if (KA_FRAGMENT.equals(fragmentName)) {
-            Intent kernelAdiutorIntent = new Intent();
-            kernelAdiutorIntent.setClassName("com.grarak.kerneladiutor", "com.grarak.kerneladiutor.activities.MainActivity");
-            startActivity(kernelAdiutorIntent);
-            finish();
-            return null;
-        }
+
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
                     + fragmentName);
